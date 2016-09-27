d0eb7ec9b7ccae07a7bc2f5753a0541385fdd0f7
weiyf <admin@weiyf.cn>
2016-09-27 16:44:01 +0800

    0.0

--- dlframe/src/main/java/cn/weiyf/dlframe/base/BaseDataBindingAdapter.java
+++ dlframe/src/main/java/cn/weiyf/dlframe/base/BaseDataBindingAdapter.java
@@ -1,4 +1,4 @@
-package cn.weiyf.dlframe.adapter;
+package cn.weiyf.dlframe.base;
 
 import android.databinding.BindingAdapter;
 import android.support.v7.widget.RecyclerView;
@@ -9,7 +9,7 @@ import cn.weiyf.dlframe.base.BaseAdapter;
  * Created by weiyf on 2016/9/27.
  */
 
-public class DataBindingAdapter {
+public class BaseDataBindingAdapter {
 
     @BindingAdapter({"adapter"})
     public static void setAdapter(RecyclerView recyclerView, BaseAdapter adapter) {
